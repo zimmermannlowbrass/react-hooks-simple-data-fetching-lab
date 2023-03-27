90137da30e3f2b4c1cfa64ff4afcaaea4b7f752b
James Aidan Zimmermann <zimmermann.lowbrass@gmail.com>
2023-03-26 19:45:19 -0500

    completed labwork

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,25 @@
 // create your App component here
+import React, { useState, useEffect } from "react";
+
+function App () {
+
+    const [dogImage, setDogImage] = useState(null)
+
+    useEffect( () =>
+        fetch(`https://dog.ceo/api/breeds/image/random`)
+        .then(resp => resp.json())
+        .then(data => setDogImage(data.message)) 
+        , []
+    )
+
+    if (!dogImage) {
+        return <p>Loading...</p>
+    }
+
+    return (
+        <img src = {dogImage} alt = 'A Random Dog' />
+    )
+}
+
+
+export default App
